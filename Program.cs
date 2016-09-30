2252c29a4d518cf83391a9efd070c932b20487a9
Jamie Myland <JM44@anglia.ac.uk>
2016-09-30 10:32:58 +0100

    Initialize
    Initialize with Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username = "Jamie";
+            Console.WriteLine("Hello " + username);
+        }
+    }
+}
